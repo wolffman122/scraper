58a2169cd42324d4b51ae59a277916eab03d1fde
Jon Wolff <jon.wolff@wolfftech.com>
2024-01-15 20:54:11 -0500

    Tailwind setup, and unit test running

---
+++ app/__tests__/page.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react"
+import Home from "../page"
+
+describe('Home Render', () => {
+  test('Render Wolff tech header', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Wolfftech's Webiste/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  })
+
+  test('Render next js version text', () => {
+    render(<Home />);
+
+    const label = screen.getByTestId('nextjs-label');
+    const version = screen.getByTestId('nextjs-version');
+
+    expect(label).toBeInTheDocument();
+    expect(version).toBeInTheDocument();
+  })
+})
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,35 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Box, Flex, Heading, Text } from '@radix-ui/themes'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col p-6">
+      <Box className='mt-5'>
+        <Heading className='text-center'>
+          Wolfftech's Webiste
+        </Heading>
+        <Flex justify={'center'}>
+        <Box className='w-80 border-2 rounded-lg'>
+          <Box>
+            <Text 
+              className='font-bold'
+              data-testid="nextjs-label">
+              Next.js current version
+            </Text>
+          </Box>
+          <Flex gap={'3'} align={'center'}>
+            <Box>
+              <Text 
+                className='font-medium'
+                data-testid="nextjs-version">
+                4
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+        </Flex>
+      </Box>
     </main>
   )
 }
